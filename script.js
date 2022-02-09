6be1dfb40703004623da15443580edf9d95fce10
anders iversen <ande397c@stud.kea.dk>
2022-02-09 20:15:41 +0100

    array counting complete

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("counting array");
+"use strict";
+
+// Variabels
+const newArr = [];
+let counter = 0;
+
+window.addEventListener("load", arrayCounter);
+
+// Init function
+
+function arrayCounter() {
+  if (counter < 22) {
+    setTimeout(addArray, 1000);
+  }
+}
+
+// Array-counter function
+
+function addArray() {
+  if (counter < 9) {
+    newArr.unshift(counter);
+    counter++;
+    console.log(newArr);
+  } else {
+    newArr.unshift(counter);
+    newArr.length = 9;
+    counter++;
+    console.log(newArr);
+  }
+
+  arrayCounter();
+}
